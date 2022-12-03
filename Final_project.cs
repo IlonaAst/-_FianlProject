9a45ece8b7c5fb12c2486e6a2f447fd3aa0e6d20
ILONA <ilonarudn@yandex.ru>
2022-12-03 22:35:08 +0300

    Программа написана

---
+++ Final_project.cs
@@ -0,0 +1,17 @@
+﻿string[] mass = {"hello", "2", "world", ":)"};
+Console.Write("["); 
+    for (int i = 0; i < mass.Length; i++)
+        {
+            if (mass[i].Length <= 3)
+            {
+                Console.Write($"'{mass[i]}'");
+                if (i < mass.Length - 2)
+                {
+                    Console.Write(", ");
+                } 
+            }
+        }
+        Console.WriteLine("]");
+      
+            
+                
